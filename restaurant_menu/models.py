bf4bb153d3583e7be4a260a7ba99df8546cf7797
Rikuto <mikadorikuto@gmail.com>
2026-01-10 16:32:37 +0900

    udpate the code

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Saladas"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts"),
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.CharField(decimial_price=2)
+    meal_type = models.CharField(choice=MEAL_TYPE)
+    author = models.ForeignKey(User)
