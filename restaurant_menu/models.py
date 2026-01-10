ffd6f44297470ffc5079d28a411e5bb93a92be41
Rikuto <mikadorikuto@gmail.com>
2026-01-10 22:58:28 +0900

    udpate the code

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+# Import Django's built-in User model for authentication and user management
 from django.contrib.auth.models import User
 
 MEAL_TYPE = (
@@ -8,10 +10,15 @@ MEAL_TYPE = (
     ("desserts", "Desserts"),
 )
 
+STATUS = ((0, "Unavaliable"), (1, "Avaliable"))
+
 
 class Item(models.Model):
     meal = models.CharField(max_length=1000, unique=True)
     description = models.CharField(max_length=2000)
     price = models.CharField(decimial_price=2)
     meal_type = models.CharField(choice=MEAL_TYPE)
-    author = models.ForeignKey(User)
+    # PROTECT prevents deletion of User if they have associated Items
+    # Other options: CASCADE (delete items), SET_NULL (set to null), SET_DEFAULT, DO_NOTHING
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choice=STATUS, default=0)
